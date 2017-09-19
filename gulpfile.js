4adbdd7dfa37b6c35b0eacb19c501c9f94bc551a
JammiM <jammim19@gmail.com>
2017-09-19 17:50:56 +0100

    Adding babel to transpile ES6 to ES5

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,41 @@
 const gulp = require('gulp');
-const uglify = require('gulp-uglify');
+const uglify = require('uglify-es');
+const pump = require('pump');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const babelCore = require('babel-core');
+const babel = require('gulp-babel');
 
 //Logs Message
 gulp.task('message',function(){
   return console.log('Gulp is running ...');
 });
 
-gulp.task('default', ['message', 'minify', 'sass']);
-
-//mininfies the .js code
-gulp.task('minify',function(){
-  gulp.src('src/js/*.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/js'));
+gulp.task('default', ['message', 'sass']);
+//gulp.task('default', ['message', 'minify', 'sass']);
+gulp.task('babel', function () {
+  return gulp.src('src/es6/main.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('src/js'));
 });
+
+
+
+/*
+//mininfies the .js code
+gulp.task('minify',function(cb){
+  pump([
+          gulp.src('src/js/*.js'),
+              uglify(),
+          gulp.dest('dist/js')
+
+//              .pipe(gulp.dest('dist/js'));
+
+  ],cb);
+});
+*/
+
 
 //Compile Scss
 gulp.task('sass',function(){
@@ -36,7 +56,7 @@ gulp.task('pug',function(){
 
 //Watch everthing
 gulp.task('watch',function(){
-  gulp.watch('src/js/*.js', ['minify']);
+//  gulp.watch('src/js/*.js', ['minify']);
   gulp.watch('src/sass/*.scss', ['sass']);
   gulp.watch('src/pug/*.pug', ['pug']);
 });
